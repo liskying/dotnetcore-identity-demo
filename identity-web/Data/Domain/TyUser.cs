eece7e73f26c908f18e65fa76e3c7aacb8515110
liskying <liwenxue1012@163.com>
2017-10-27 12:04:02 +0800

    'TyUser主键类型变更'

--- identity-web/Data/Domain/TyUser.cs
+++ identity-web/Data/Domain/TyUser.cs
@@ -1,15 +1,17 @@
-﻿using System; 
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq; 
-using System.Threading.Tasks; 
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 
-namespace identity_web.Data.Domain {
+namespace identity_web.Data.Domain
+{
     /// <summary>
     /// 用户
     /// </summary>
     [Table("TyUser")]
-    public class TyUser:IdentityUser {
-}
+    public class TyUser : IdentityUser<string>
+    {
+    }
 }
